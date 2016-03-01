547289176cca23456365eb6d3a9b099fccab61ee
julianzaugg <julianzaugg@gmail.com>
2016-03-01 23:03:12 +1000

    more processing options added to improve compatability of pdbqt files with autodock vina

--- to_pdbqt.sh
+++ to_pdbqt.sh
@@ -4,5 +4,5 @@ OUTLOCATION=""
 for f in PDBLOCATION/*.pdb; do
 	b=`basename $f .pdb`
 	echo Converting pdb $b
-	prepare_receptor4.py -r $f -o OUTLOCATION/${b}.pdbqt -A checkhydrogens -U waters
+	prepare_receptor4.py -r $f -o OUTLOCATION/${b}.pdbqt -A bond_hydrogens -U nphs_lps_waters_nonstdres_deleteAltB
 done
